0099186374f8ba777ac25df35764249b47b13c3e
Emre Akyol <if24b047@technikum-wien.at>
2025-10-10 15:03:47 +0200

    DMS Domain, project + .cs Datei

---
+++ DocumentManagementSystem/DMS.Domain/Entities/Document.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DMS.Domain.Entities
+{
+    public class Document
+    {
+        public int Id { get; set; }
+        public string FileName { get; set; }
+        public string ContentType { get; set; }
+        public byte[] Data { get; set; }  // Or use string for path if storing files externally
+        public DateTime UploadedAt { get; set; }
+        // Add more fields like Tags, Summary later in other sprints
+    }
+}
