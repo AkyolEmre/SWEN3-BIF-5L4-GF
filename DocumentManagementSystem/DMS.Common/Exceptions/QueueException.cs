ee1d5c4f82bcbb385b9597f1a864eb4f84559c1f
Emre Akyol <if24b047@technikum-wien.at>
2025-11-13 19:34:56 +0100

    DMS.Common

---
+++ DocumentManagementSystem/DMS.Common/Exceptions/QueueException.cs
@@ -0,0 +1,8 @@
+﻿namespace DMS.Common.Exceptions
+{
+    public class QueueException : Exception
+    {
+        public QueueException(string message) : base(message) { }
+        public QueueException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
