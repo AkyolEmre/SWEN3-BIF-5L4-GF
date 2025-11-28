f3824c5954927b642957d9a063460fdd2c3a19c1
Emre Akyol <if24b047@technikum-wien.at>
2025-11-28 13:07:06 +0100

    DMSDomain

--- DocumentManagementSystem/DMS.Domain/Entities/Document.cs
+++ DocumentManagementSystem/DMS.Domain/Entities/Document.cs
@@ -1,14 +1,19 @@
-﻿using System;
-
-namespace DMS.Domain.Entities
+﻿namespace DMS.Domain.Entities
 {
     public class Document
     {
         public int Id { get; set; }
         public string FileName { get; set; } = string.Empty;
         public string ContentType { get; set; } = string.Empty;
-        public byte[] Data { get; set; } = Array.Empty<byte>();
+
+        // Alt: Data als Byte[] (Sprint 1-3)
+        public byte[]? Data { get; set; }
+
+        // NEU: MinIO Object Name (Sprint 4)
+        public string? MinIOObjectName { get; set; }
+
         public DateTime UploadedAt { get; set; }
-        public DocumentStatus Status { get; set; }
+        public DocumentStatus Status { get; set; } = DocumentStatus.PendingOcr;
     }
+
 }
