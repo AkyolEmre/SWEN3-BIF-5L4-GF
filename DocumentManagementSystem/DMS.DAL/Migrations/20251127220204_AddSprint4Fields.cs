182df201e1e0db795afad9948a4973c8f2166e9b
Emre Akyol <if24b047@technikum-wien.at>
2025-11-28 13:07:18 +0100

    DMS.DAL

---
+++ DocumentManagementSystem/DMS.DAL/Migrations/20251127220204_AddSprint4Fields.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DMS.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSprint4Fields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "Data",
+                table: "Documents",
+                type: "bytea",
+                nullable: true,
+                oldClrType: typeof(byte[]),
+                oldType: "bytea");
+
+            migrationBuilder.AddColumn<string>(
+                name: "MinIOObjectName",
+                table: "Documents",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "Documents",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MinIOObjectName",
+                table: "Documents");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Documents");
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "Data",
+                table: "Documents",
+                type: "bytea",
+                nullable: false,
+                defaultValue: new byte[0],
+                oldClrType: typeof(byte[]),
+                oldType: "bytea",
+                oldNullable: true);
+        }
+    }
+}
---
+++ DocumentManagementSystem/DMS.DAL/Migrations/20251127222349_AddSprint4_MinioFix.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DMS.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSprint4_MinioFix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
